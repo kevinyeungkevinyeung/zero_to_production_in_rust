31cdf9253cab79cd4656e23286e07e00c73e3c5d
kevinyeungkevinyeung <kevinyeungkevinyeung@hotmail.com>
2024-05-20 22:04:28 +0800

    Create a health check api

---
+++ zero2prod/src/lib.rs
@@ -0,0 +1,26 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+// we beed to mark 'run' as public.
+// It is no longer a binary entrypoint, therefore we acn market it as async
+// without having to use any proc-macro incantation
+#[tokio::main]
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
